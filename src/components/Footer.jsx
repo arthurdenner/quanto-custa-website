6c4de3597d50ae6ad79e32a74641dc55ff33eab4
Arthur Denner <arthurdenner7@gmail.com>
2019-12-27 23:09:06 +0100

    feat(footer): add icons without links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 /**@jsx jsx */
-import { Footer as ThemeFooter, jsx } from 'theme-ui';
+import { Footer as ThemeFooter, jsx, Flex } from 'theme-ui';
+import { Download, Mail, Facebook, Instagram } from '../elements/FooterIcons';
 import Logo from '../elements/Logo';
 
 const Footer = () => (
   <ThemeFooter sx={{ backgroundColor: `light`, p: 2 }}>
     <Logo />
+    <Flex sx={{ justifyContent: `space-around`, mt: 4 }}>
+      <Download />
+      <Mail />
+      <Facebook />
+      <Instagram />
+    </Flex>
   </ThemeFooter>
 );
 
