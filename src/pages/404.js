934e55e5c929b87977b683c35fe11183fd9e5d10
Arthur Denner <arthurdenner7@gmail.com>
2019-12-28 21:19:37 +0100

    feat(pages): implement not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,36 @@
-import React from 'react';
+/** @jsx jsx */
+import { Link } from 'gatsby';
+import { jsx, Box, Flex } from 'theme-ui';
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import NotFound from '../elements/NotFound';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Página não encontrada" />
+    <Flex
+      sx={{
+        flexDirection: `column`,
+        justifyContent: `center`,
+        alignItems: `center`,
+        height: `100%`,
+        p: 4,
+      }}
+    >
+      <Box sx={{ maxWidth: 400, width: `full` }}>
+        <NotFound />
+      </Box>
+      <p>Parece que você está perdido</p>
+      <Link
+        sx={{
+          variant: `buttons.transparent`,
+          textDecoration: `underline`,
+        }}
+        to="/"
+      >
+        Voltar à página inicial
+      </Link>
+    </Flex>
   </Layout>
 );
 
