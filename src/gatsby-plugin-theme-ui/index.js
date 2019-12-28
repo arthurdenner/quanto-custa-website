8f1599b130ab6191e05b9128e9195f9022ca2444
Arthur Denner <arthurdenner7@gmail.com>
2019-12-28 21:43:46 +0100

    refactor(theme): remove unnecessary config

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,14 +1,5 @@
 import { tailwind } from '@theme-ui/presets';
 
-const sharedButtonStyles = {
-  border: `none`,
-  color: `white`,
-  cursor: `pointer`,
-  fontFamily: `body`,
-  fontWeight: `medium`,
-  transition: `all 0.3s ease-in-out`,
-};
-
 const sharedHeadingStyles = {
   color: `heading`,
 };
@@ -20,39 +11,16 @@ export default {
   colors: {
     ...tailwind.colors,
     primary: `#FDFFFF`,
-    shadow: tailwind.colors.indigo[2],
-    secondary: tailwind.colors.orange[5],
     background: `#562F58`,
-    backgroundThemes: tailwind.colors.white,
     textMuted: `#B6B6B6`,
     text: `#FDFFFF`,
-    grayDark: tailwind.colors.gray[9],
     heading: `#FDFFFF`,
-    dark: tailwind.colors.gray[7],
-    muted: tailwind.colors.gray[5],
     light: `hsla(297, 30%, 46%, 0.5)`,
-    model: {
-      one: tailwind.colors.gray[2],
-      two: tailwind.colors.gray[3],
-      three: tailwind.colors.gray[4],
-      four: tailwind.colors.gray[5],
-    },
     modes: {
       dark: {
-        shadow: tailwind.colors.indigo[8],
         background: `#212121`,
-        backgroundThemes: tailwind.colors.gray[8],
         textMuted: `#B6B6B6`,
-        grayDark: tailwind.colors.gray[1],
-        // heading: tailwind.colors.gray[2],
-        dark: tailwind.colors.gray[5],
         light: `hsla(0, 0%, 23%, 0.5)`,
-        model: {
-          one: tailwind.colors.gray[8],
-          two: tailwind.colors.gray[7],
-          three: tailwind.colors.gray[6],
-          four: tailwind.colors.gray[5],
-        },
       },
     },
   },
@@ -77,10 +45,6 @@ export default {
     `16rem`,
     `32rem`,
   ],
-  radii: {
-    ...tailwind.radii,
-    xl: `1rem`,
-  },
   styles: {
     ...tailwind.styles,
     Footer: {
@@ -139,75 +103,6 @@ export default {
     },
   },
   buttons: {
-    primary: {
-      ...sharedButtonStyles,
-      borderRadius: `full`,
-      backgroundImage: t =>
-        `linear-gradient(45deg, ${t.colors.indigo[6]}, ${t.colors.indigo[4]})`,
-      fontSize: [1, 2],
-      px: 4,
-      py: `0.6rem`,
-      boxShadow: t => `0px 10px 15px ${t.colors.shadow}`,
-      '&:hover': {
-        transform: `translateY(-2px)`,
-        boxShadow: t => `0px 20px 25px ${t.colors.shadow}`,
-      },
-    },
-    secondary: {
-      ...sharedButtonStyles,
-      borderRadius: `full`,
-      backgroundImage: t =>
-        `linear-gradient(45deg, ${t.colors.gray[7]}, ${t.colors.gray[5]})`,
-      fontSize: 1,
-      px: 4,
-      py: 1,
-      boxShadow: `default`,
-      '&:hover': {
-        transform: `translateY(-1px)`,
-      },
-    },
-    newsletter: {
-      ...sharedButtonStyles,
-      borderRadius: `default`,
-      px: `1.25rem`,
-      py: `0.6rem`,
-      backgroundColor: `primary`,
-      fontSize: 1,
-      boxShadow: `default`,
-      '&:hover': {
-        transform: `translateY(-1px)`,
-      },
-    },
-    outline: {
-      ...sharedButtonStyles,
-      backgroundColor: `transparent`,
-      borderWidth: 2,
-      borderColor: `white`,
-      borderStyle: `solid`,
-      borderRadius: `full`,
-      textAlign: `center`,
-      fontSize: 1,
-      fontWeight: `semibold`,
-      px: 4,
-      py: `0.7rem`,
-      '&:hover': {
-        backgroundColor: `white`,
-        color: `primary`,
-      },
-    },
-    white: {
-      ...sharedButtonStyles,
-      borderRadius: `full`,
-      fontSize: 1,
-      fontWeight: `semibold`,
-      px: 4,
-      py: `0.75rem`,
-      backgroundColor: `white`,
-      color: `primary`,
-      '&:hover': {
-        transform: `translateY(-1px)`,
-      },
-    },
     transparent: {
       color: `inherit`,
       bg: `transparent`,
@@ -219,73 +114,6 @@ export default {
         boxShadow: `0 0 0 2px`,
         cursor: `pointer`,
       },
-    },
-  },
-  inputs: {
-    primary: {
-      px: 3,
-      py: `0.6rem`,
-      fontSize: 1,
-      borderRadius: `default`,
-      boxShadow: `default`,
-      border: `none`,
-      outline: `none`,
-      '&:focus': {
-        boxShadow: `outline`,
-      },
-    },
-  },
-  gradients: {
-    blue: {
-      backgroundImage: t =>
-        `linear-gradient(45deg, ${t.colors.indigo[3]}, ${t.colors.indigo[5]})`,
-    },
-    orange: {
-      backgroundImage: t =>
-        `linear-gradient(225deg, ${t.colors.orange[3]}, ${t.colors.orange[5]})`,
-    },
-    pink: {
-      backgroundImage: t =>
-        `linear-gradient(135deg, ${t.colors.pink[4]}, ${t.colors.pink[6]})`,
-    },
-    purple: {
-      backgroundImage: t =>
-        `linear-gradient(135deg, ${t.colors.purple[6]}, ${t.colors.purple[3]})`,
-    },
-    gray: {
-      backgroundImage: t =>
-        `linear-gradient(135deg, ${t.colors.gray[5]}, ${t.colors.gray[7]})`,
-    },
-    black: {
-      backgroundImage: t =>
-        `linear-gradient(135deg, ${t.colors.gray[7]}, ${t.colors.gray[9]})`,
-    },
-    red: {
-      backgroundImage: t =>
-        `linear-gradient(225deg, ${t.colors.red[6]}, ${t.colors.red[3]})`,
-    },
-    yellow: {
-      backgroundImage: t =>
-        `linear-gradient(180deg, ${t.colors.yellow[5]}, ${t.colors.yellow[6]})`,
-    },
-    green: {
-      backgroundImage: t =>
-        `linear-gradient(225deg, ${t.colors.green[3]}, ${t.colors.green[5]})`,
-    },
-    indigo: {
-      backgroundImage: t =>
-        `linear-gradient(72deg, ${t.colors.indigo[7]}, ${t.colors.indigo[5]})`,
-    },
-    teal: {
-      backgroundImage: t =>
-        `linear-gradient(72deg, ${t.colors.teal[7]}, ${t.colors.teal[5]})`,
-    },
-    darkIndigo: {
-      backgroundImage: t =>
-        `linear-gradient(135deg, ${t.colors.indigo[8]}, ${t.colors.indigo[6]})`,
-    },
-    strangerThings: {
-      backgroundImage: `radial-gradient(#BA230D, #520506)`,
     },
   },
 };
