521edb7905a1228866d7c34c4a019b5aa4318c3c
Arthur Denner <arthurdenner7@gmail.com>
2019-12-28 11:03:13 +0100

    feat(footer): add links to elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,26 @@
 /**@jsx jsx */
+import { Link } from 'gatsby';
 import { Footer as ThemeFooter, jsx, Flex } from 'theme-ui';
+import ExternalLink from '../elements/ExternalLink';
 import { Download, Mail, Facebook, Instagram } from '../elements/FooterIcons';
 import Logo from '../elements/Logo';
 
 const Footer = () => (
-  <ThemeFooter sx={{ backgroundColor: `light`, p: 2 }}>
+  <ThemeFooter sx={{ backgroundColor: `light` }}>
     <Logo />
     <Flex sx={{ justifyContent: `space-around`, mt: 4 }}>
-      <Download />
-      <Mail />
-      <Facebook />
-      <Instagram />
+      <Link to="/#download">
+        <Download />
+      </Link>
+      <ExternalLink href="mailto:quanto.custa.app@gmail.com">
+        <Mail />
+      </ExternalLink>
+      <ExternalLink href="https://facebook.com/quantocusta.me">
+        <Facebook />
+      </ExternalLink>
+      <ExternalLink href="https://instagram.com/quantocusta.me">
+        <Instagram />
+      </ExternalLink>
     </Flex>
   </ThemeFooter>
 );
