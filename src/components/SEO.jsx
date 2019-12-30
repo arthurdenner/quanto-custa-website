dbe3f046f322a7015fa7cf798d2ac1aedfd80d54
Arthur Denner <arthurdenner7@gmail.com>
2019-12-30 15:54:11 +0100

    feat(seo): add extra meta tags for Facebook

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -19,6 +19,7 @@ function SEO({ description, title, titleTemplate }) {
       <html lang={siteMetadata.siteLanguage} />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.socialImage} />
+      <meta property="fb:app_id" content="1485607778252911" />
 
       <meta property="og:site_name" content={siteMetadata.facebook} />
       <meta property="og:locale" content={siteMetadata.ogLanguage} />
@@ -28,6 +29,8 @@ function SEO({ description, title, titleTemplate }) {
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.socialImage} />
       <meta property="og:image:alt" content={seo.description} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
 
       <meta name="twitter:author" content={siteMetadata.author} />
       <meta name="twitter:card" content="summary" />
