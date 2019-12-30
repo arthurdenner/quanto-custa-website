fb8b893b400bcdd39d9891e86c7dd71dc728ceb1
Arthur Denner <arthurdenner7@gmail.com>
2019-12-30 13:46:48 +0100

    feat(seo): add og_image and config file

---
+++ config/website.js
@@ -0,0 +1,13 @@
+module.exports = {
+  author: `@quantocusta_me`,
+  title: `Quanto Custa`,
+  shortDescription: `Seu aplicativo diário na hora de economizar.`,
+  longDescription: `Aplicativo gratuito onde alagoanos podem buscar por produtos e combustíveis, visualizar onde é mais barato encontrar o que precisam e como chegar até lá.`,
+  image: `/img/og-image.jpg`,
+  siteLanguage: `pt-br`,
+  ogLanguage: `pt_BR`,
+  siteUrl: `https://quantocusta.me`,
+  facebook: `https://facebook.com/quantocusta.me`,
+  instagram: `https://instagram.com/quantocusta.me`,
+  email: `quanto.custa.app@gmail.com`,
+};
