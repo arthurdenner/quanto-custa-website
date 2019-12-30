4dde534bb4046849429165eb5c65f26d90527f16
Arthur Denner <arthurdenner7@gmail.com>
2019-12-30 13:48:24 +0100

    feat(seo): update SEO, Hero and Footer components to use new data

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,7 @@
 /**@jsx jsx */
 import { Link } from 'gatsby';
 import { Footer as ThemeFooter, jsx, Flex, Container } from 'theme-ui';
+import useSiteMetadata from '../../hooks/useSiteMetadata';
 import ExternalLink from '../ExternalLink';
 import Logo from '../Logo';
 import { Download, Mail, Facebook, Instagram } from './icons';
@@ -11,59 +12,59 @@ const linkStyle = {
   p: [0, 0, 3],
 };
 
-const Footer = () => (
-  <ThemeFooter
-    sx={{
-      backgroundColor: `light`,
-    }}
-  >
-    <Container
-      sx={{
-        display: `flex`,
-        flexDirection: [`column`, `column`, `row`],
-        justifyContent: [null, null, `space-between`],
-        alignItems: `center`,
-      }}
-    >
-      <Logo id="footer" />
-      <Flex
+const Footer = () => {
+  const siteMetadata = useSiteMetadata();
+
+  return (
+    <ThemeFooter sx={{ backgroundColor: `light` }}>
+      <Container
         sx={{
-          justifyContent: `space-around`,
-          width: [`full`, `full`, `auto`],
-          mt: [4, 4, 0],
+          display: `flex`,
+          flexDirection: [`column`, `column`, `row`],
+          justifyContent: [null, null, `space-between`],
+          alignItems: `center`,
         }}
       >
-        <Link
-          aria-label="Link para seçäo de download do aplicativo"
-          to="/#download"
-          sx={linkStyle}
+        <Logo id="footer" />
+        <Flex
+          sx={{
+            justifyContent: `space-around`,
+            width: [`full`, `full`, `auto`],
+            mt: [4, 4, 0],
+          }}
         >
-          <Download />
-        </Link>
-        <ExternalLink
-          aria-label="Enviar e-mail para o Quanto Custa"
-          href="mailto:quanto.custa.app@gmail.com"
-          sx={linkStyle}
-        >
-          <Mail />
-        </ExternalLink>
-        <ExternalLink
-          aria-label="Ver página do Quanto Custa no Facebook"
-          href="https://facebook.com/quantocusta.me"
-          sx={linkStyle}
-        >
-          <Facebook />
-        </ExternalLink>
-        <ExternalLink
-          aria-label="Ver página do Quanto Custa no Instagram"
-          href="https://instagram.com/quantocusta.me"
-          sx={linkStyle}
-        >
-          <Instagram />
-        </ExternalLink>
-      </Flex>
-    </Container>
-  </ThemeFooter>
-);
+          <Link
+            aria-label="Link para seçäo de download do aplicativo"
+            to="/#download"
+            sx={linkStyle}
+          >
+            <Download />
+          </Link>
+          <ExternalLink
+            aria-label="Enviar e-mail para o Quanto Custa"
+            href={`mailto:${siteMetadata.email}`}
+            sx={linkStyle}
+          >
+            <Mail />
+          </ExternalLink>
+          <ExternalLink
+            aria-label="Ver página do Quanto Custa no Facebook"
+            href={siteMetadata.facebook}
+            sx={linkStyle}
+          >
+            <Facebook />
+          </ExternalLink>
+          <ExternalLink
+            aria-label="Ver página do Quanto Custa no Instagram"
+            href={siteMetadata.instagram}
+            sx={linkStyle}
+          >
+            <Instagram />
+          </ExternalLink>
+        </Flex>
+      </Container>
+    </ThemeFooter>
+  );
+};
 
 export default Footer;
--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,64 +3,50 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import useSiteMetadata from '../hooks/useSiteMetadata';
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, title, titleTemplate }) {
   const siteMetadata = useSiteMetadata();
-  const metaDescription = description || siteMetadata.description;
+  const seo = {
+    title,
+    titleTemplate: titleTemplate || `%s | ${siteMetadata.title}`,
+    description: description || siteMetadata.longDescription,
+    image: `${siteMetadata.siteUrl}${siteMetadata.image}`,
+    url: `${siteMetadata.siteUrl}`,
+  };
 
   return (
-    <Helmet
-      htmlAttributes={{ lang }}
-      title={title}
-      titleTemplate={`%s | ${siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    />
+    <Helmet title={seo.title} titleTemplate={seo.titleTemplate}>
+      <html lang={siteMetadata.siteLanguage} />
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+
+      <meta property="og:site_name" content={siteMetadata.facebook} />
+      <meta property="og:locale" content={siteMetadata.ogLanguage} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:image:alt" content={seo.description} />
+
+      <meta name="twitter:author" content={siteMetadata.author} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:image:alt" content={seo.description} />
+    </Helmet>
   );
 }
 
 SEO.defaultProps = {
-  lang: `pt-br`,
-  meta: [],
   description: ``,
+  titleTemplate: ``,
 };
 
 SEO.propTypes = {
   description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
+  titleTemplate: PropTypes.string,
 };
 
 export default SEO;
