7043b04acc90f612470fc63c8f7c01a5903e4d68
Arthur Denner <arthurdenner7@gmail.com>
2020-01-03 16:44:05 +0100

    fix(footer): add link to privacy policy page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 /**@jsx jsx */
 import { Link } from 'gatsby';
-import { Footer as ThemeFooter, jsx, Flex, Container } from 'theme-ui';
+import { Box, Footer as ThemeFooter, jsx, Flex, Container } from 'theme-ui';
 import useSiteMetadata from '../../hooks/useSiteMetadata';
 import ExternalLink from '../ExternalLink';
 import Logo from '../Logo';
@@ -17,53 +17,57 @@ const Footer = () => {
 
   return (
     <ThemeFooter sx={{ backgroundColor: `light` }}>
-      <Container
-        sx={{
-          display: `flex`,
-          flexDirection: [`column`, `column`, `row`],
-          justifyContent: [null, null, `space-between`],
-          alignItems: `center`,
-          pt: [4, 4, 5],
-          pb: [4, 4, 5],
-        }}
-      >
-        <Logo id="footer" />
-        <Flex
+      <Container sx={{ pt: [4, 4, 5], pb: [4, 4, 5] }}>
+        <Box
           sx={{
-            justifyContent: `space-around`,
-            width: [`full`, `full`, `auto`],
-            mt: [4, 4, 0],
+            display: `flex`,
+            flexDirection: [`column`, `column`, `row`],
+            justifyContent: [null, null, `space-between`],
+            alignItems: `center`,
+            mb: 4,
           }}
         >
-          <Link
-            aria-label="Link para seçäo de download do aplicativo"
-            to="/#download"
-            sx={linkStyle}
+          <Logo id="footer" />
+          <Flex
+            sx={{
+              justifyContent: `space-around`,
+              width: [`full`, `full`, `auto`],
+              mt: [4, 4, 0],
+            }}
           >
-            <Download />
-          </Link>
-          <ExternalLink
-            aria-label="Enviar e-mail para o Quanto Custa"
-            href={`mailto:${siteMetadata.email}`}
-            sx={linkStyle}
-          >
-            <Mail />
-          </ExternalLink>
-          <ExternalLink
-            aria-label="Ver página do Quanto Custa no Facebook"
-            href={siteMetadata.facebook}
-            sx={linkStyle}
-          >
-            <Facebook />
-          </ExternalLink>
-          <ExternalLink
-            aria-label="Ver página do Quanto Custa no Instagram"
-            href={siteMetadata.instagram}
-            sx={linkStyle}
-          >
-            <Instagram />
-          </ExternalLink>
-        </Flex>
+            <Link
+              aria-label="Link para seçäo de download do aplicativo"
+              to="/#download"
+              sx={linkStyle}
+            >
+              <Download />
+            </Link>
+            <ExternalLink
+              aria-label="Enviar e-mail para o Quanto Custa"
+              href={`mailto:${siteMetadata.email}`}
+              sx={linkStyle}
+            >
+              <Mail />
+            </ExternalLink>
+            <ExternalLink
+              aria-label="Ver página do Quanto Custa no Facebook"
+              href={siteMetadata.facebook}
+              sx={linkStyle}
+            >
+              <Facebook />
+            </ExternalLink>
+            <ExternalLink
+              aria-label="Ver página do Quanto Custa no Instagram"
+              href={siteMetadata.instagram}
+              sx={linkStyle}
+            >
+              <Instagram />
+            </ExternalLink>
+          </Flex>
+        </Box>
+        <Box>
+          <Link to="/politica-de-privacidade">Política de Privacidade</Link>
+        </Box>
       </Container>
     </ThemeFooter>
   );
