d2381d54bb6f8e97753bfd80987dd9d078c70aee
Arthur Denner <arthurdenner7@gmail.com>
2020-01-04 22:06:17 +0100

    refactor(hero): use ExternalLink and add variables to hold urls

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -2,30 +2,29 @@
 import { Box, jsx, Styled } from 'theme-ui';
 import appStoreBadge from '../images/app_store_badge.png';
 import playStoreBadge from '../images/play_store_badge.png';
+import ExternalLink from './ExternalLink';
+
+const appStoreLink = `/`;
+const playStoreLink = `/`;
 
 const PlayStoreBadge = () => (
-  <Styled.a
-    href="/"
-    target="_blank"
-    rel="noopener noreferrer"
-    sx={{ height: `40px` }}
-  >
+  <ExternalLink href={playStoreLink} sx={{ height: `40px` }}>
     <Styled.img
       aria-label="Link para download do aplicativo na Play Store"
       src={playStoreBadge}
       sx={{ maxWidth: [`100%`, `135px`] }}
     />
-  </Styled.a>
+  </ExternalLink>
 );
 
 const AppStoreBadge = () => (
-  <Styled.a href="/" target="_blank" sx={{ height: `40px`, ml: [null, 3] }}>
+  <ExternalLink href={appStoreLink} sx={{ height: `40px`, ml: [null, 3] }}>
     <Styled.img
       aria-label="Link para download do aplicativo na App Store"
       src={appStoreBadge}
       sx={{ maxWidth: [`100%`, `135px`] }}
     />
-  </Styled.a>
+  </ExternalLink>
 );
 
 const Badges = () => (
